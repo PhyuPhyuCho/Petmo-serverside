f0862a040ab19ecf093fc7cb407169abe1788bc3
PhyuPhyuCho <44861879+PhyuPhyuCho@users.noreply.github.com>
2023-08-25 11:07:16 +0900

    test

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,20 @@
 <?php
 
-//use Illuminate\Support\Facades\Route;
-//use App\Http\Controllers;
+use Illuminate\Support\Facades\Route;
 
 
-//Route::get('/pet-azuke-places', '\App\Http\Controllers\AzukePlaceController@getAllPetAzukePlaces');
-//Route::get('/pet-azuke-places/{place_id}', '\App\Http\Controllers\AzukePlaceController@getPetAzukePlace');
-//Route::post('/api/chat', '\App\Http\Controllers\ChatController@sendMessage')->name('api.chat.send');
-//Route::get('/chat', '\App\Http\Controllers\ChatController@sendMessage')->name('api.chat.send');
-//Route::get('/chat/{user_id}', '\App\Http\Controllers\ChatController@fetchMessages');
+// Login and Logout Routes
+Route::get('/login', '\App\Http\Controllers\AuthController@showLoginForm')->name('login');
+Route::post('/login', '\App\Http\Controllers\AuthController@login');
+Route::post('/logout', '\App\Http\Controllers\AuthController@logout')->name('logout');
+
+// Signup Routes
+Route::get('/signup', '\App\Http\Controllers\AuthController@showSignupForm')->name('signup');
+Route::post('/signup', '\App\Http\Controllers\AuthController@signup');
+
+
+Route::post('/chat/{sender_user_id}/{receiver_user_id}/{place_id}', '\App\Http\Controllers\ChatController@sendMessage')->name('api.chat.send');
+Route::get('/chat/{sender_user_id}/{receiver_user_id}/{place_id}', '\App\Http\Controllers\ChatController@fetchMessagesFromMe')->name('api.chat.fetch');
+
+Route::get('/pet-azuke-places', '\App\Http\Controllers\AzukePlaceController@getAllPetAzukePlaces')->name('api.petallazukeplaces.fetch');
+Route::get('/pet-azuke-places/{place_id}', '\App\Http\Controllers\AzukePlaceController@getPetAzukePlace')->name('getPetAzukePlace');
